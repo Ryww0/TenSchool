fd35452a8849d4b3cb968bf85a3c6acadf662d4f
Ryww0 <laurent.chaitas@gmail.com>
2022-10-19 12:10:49 +0200

    SubjectController :: created & showSubject()

---
+++ src/Controller/SubjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Subject;
+use App\Repository\SubjectRepository;
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SubjectController extends AbstractController {
+    #[Route('subjects/{subject}', name: 'app_subjects_subject')]
+    public function showSubject(Subject $subject, SubjectRepository $subjectRepository): Response
+    {
+        $s = $subjectRepository->find($subject);
+
+        return $this->render('subject/subject.html.twig', [
+            'subject' => $s,
+        ]);
+    }
+}
